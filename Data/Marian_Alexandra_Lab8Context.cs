d45be0b52584f962727e1816b9a1fc18b1f21ac1
marian alexandra <marianalexandra2602@yahoo.com>
2021-11-22 12:48:50 +0200

    Add project files.

---
+++ Data/Marian_Alexandra_Lab8Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Marian_Alexandra_Lab8.Models;
+
+namespace Marian_Alexandra_Lab8.Data
+{
+    public class Marian_Alexandra_Lab8Context : DbContext
+    {
+        public Marian_Alexandra_Lab8Context (DbContextOptions<Marian_Alexandra_Lab8Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Marian_Alexandra_Lab8.Models.Book> Book { get; set; }
+
+        public DbSet<Marian_Alexandra_Lab8.Models.Publisher> Publisher { get; set; }
+
+        public DbSet<Marian_Alexandra_Lab8.Models.Category> Category { get; set; }
+    }
+}
